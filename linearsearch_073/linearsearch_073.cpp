1afe81e8e53e5d899d7528925cad386b681b910d
zil-web <126854462+zil-web@users.noreply.github.com>
2023-04-14 14:43:17 +0700

    Enter the number of elements in the array

--- linearsearch_073/linearsearch_073.cpp
+++ linearsearch_073/linearsearch_073.cpp
@@ -4,3 +4,12 @@ using namespace std;
 int arr[20]; //array to be search
 int n; // Number of elemnts in the array
 int i; // Index of array element
+
+void input()
+{
+	while (true)
+	{
+		cout << "Enter the number of elements in the array : ";
+
+	}
+}
